8b9268e146c348892a378c672ba56daa502c1c64
NAZIANO FELIX DE ALMEIDA JUNIOR <a9187448@rn.senac.br>
2019-08-14 19:29:16 -0300

    aula agenda contatoview

--- src/br/senac/rn/agenda/view/ContatoView.java
+++ src/br/senac/rn/agenda/view/ContatoView.java
@@ -2,7 +2,31 @@ package br.senac.rn.agenda.view;
 
 import br.senac.rn.agenda.model.Contato;
 
+import java.util.Scanner;
+
 public class ContatoView {
 
+    private static Scanner entrada = new Scanner(System.in);
 
+    public static void main(String[] args) {
+
+        Long escolha = 1L;
+
+        do {
+            ContatoView.getMenu();
+            escolha = entrada.nextLong();
+        } while (escolha != 0);
+
+    }
+
+    private static void getMenu() {
+        String menu = "";
+        menu += "==============================\n";
+        menu += "1 - Cadastrar Contato         \n";
+        menu += "2 - Listar Todos os Contatos  \n";
+        menu += "3 - Buscar Contato            \n";
+        menu += "==============================\n";
+        menu += "Opção: ";
+        System.out.print(menu);
+    }
 }
